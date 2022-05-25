40f05287c5241d30b1be89b72a017bd8e9af51b0
jckling <jckling@163.com>
2022-05-25 21:03:14 +0800

    part1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,33 +6,27 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
-const Display = ({value, text}) => (
+const StatisticLine = ({text, value}) => (
   <div>
     {text} {value}
   </div>
 )
 
-const Average = ({good, neutral, bad}) => {
-  const sum = good + neutral + bad
-  const avg = (good - bad) / sum
-  return <div>average {avg}</div>
-}
-
-const Positive = ({good, neutral, bad}) => {
-  const sum = good + neutral + bad
-  const pos = good / sum
-  return <div>positive {pos} %</div>
-}
-
 const Statistics = ({good, neutral, bad}) => {
   if (good === 0 && neutral === 0 && bad === 0) return <div>No feedback given</div>
+
+  const sum = good + neutral + bad
+  const average = () => (good - bad) / sum
+  const positive = () => good / sum * 100 + " %"
+
   return (
     <div>
-      <Display value={good} text="good" />
-      <Display value={neutral} text="neutral" />
-      <Display value={bad} text="bad" />
-      <Average good={good} neutral={neutral} bad={bad} />
-      <Positive good={good} neutral={neutral} bad={bad} />
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={sum} />
+      <StatisticLine text="average" value={average()} />
+      <StatisticLine text="positive" value={positive()} />
     </div>
   )
 }
