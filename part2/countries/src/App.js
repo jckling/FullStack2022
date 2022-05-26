d7843c72a78fdf2647e262bce2d65a5a9d25a54f
jckling <jckling@163.com>
2022-05-26 19:25:10 +0800

    part2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,28 +2,34 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios' 
 
 const Country = ({country}) => {
-  return (
-    <div>
-      <h1>{country.name.common}</h1>
-      <p>capital {country.capital}</p>
-      <p>population {country.population}</p>
-      <h2>languages</h2>
-      <ul>
-        {Object.keys(country.languages).map(key => <li key={key}>{country.languages[key]}</li>)}
-      </ul>
-      <img src={country.flags.png} />
-    </div>
-  )
+  if (country) {
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h2>languages</h2>
+        <ul>
+          {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+        </ul>
+        <img src={country.flags.png} />
+      </div>
+    )
+  }
 }
 
 const App = () => {
   const [name, setName] = useState('')
   const [countries, setCountries] = useState([])
+  const [showCountry, setCountry] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setName(event.target.value.toLowerCase())
+    setCountry('')
   }
+
+  const handleShowCountry = (country) => setCountry(country)
 
   useEffect(() => {
     axios
@@ -38,7 +44,17 @@ const App = () => {
     if (name === '') return
     const filterCountries = countries.filter(country => country.name.common.toLowerCase().includes(name))
     if (filterCountries.length > 10)  return <p>Too many matches, specify another filter</p>
-    if (filterCountries.length > 1)   return filterCountries.map(country => <p key={country.name.common}>{country.name.common}</p>)
+    if (filterCountries.length > 1)   return (
+      <div>
+        {filterCountries.map(country => (
+            <div key={country.name.common}>
+              {country.name.common} <button onClick={() => {handleShowCountry(country)} }>show</button>
+            </div>
+          )
+        )}
+        <Country country={showCountry} />
+      </div>
+    )
     if (filterCountries.length === 1) return <Country country={filterCountries[0]} />
   }
 
