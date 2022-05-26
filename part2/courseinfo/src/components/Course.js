f2b44b4cac6def35327673ace07084a9213b6edb
jckling <jckling@163.com>
2022-05-26 11:33:46 +0800

    part2.2 part2.3

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -4,6 +4,11 @@ const Header = ({name}) => <h1>{name}</h1>
 
 const Content = ({part}) => <p>{part.name} {part.exercises}</p>
 
+const Total = ({parts}) => {
+  const result = parts.reduce((prev, curr) => prev = prev + curr.exercises, 0)
+  return <p><b> total of {result} exercises</b></p>
+}
+
 const Course = ({course}) => {
   return (
     <div>
@@ -11,6 +16,7 @@ const Course = ({course}) => {
       {course.parts.map(part => 
         <Content key={part.id} part={part} />
       )}
+      <Total parts={course.parts} />
     </div>
   )
 }
