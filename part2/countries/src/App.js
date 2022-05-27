0ef3b1e51552e0d2663b3ba057ad9fb20b41e2ba
jckling <jckling@163.com>
2022-05-27 14:38:33 +0800

    part2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -8,11 +8,40 @@ const Country = ({country}) => {
         <h1>{country.name.common}</h1>
         <p>capital {country.capital}</p>
         <p>population {country.population}</p>
-        <h2>languages</h2>
+        <h2>Spoken languages</h2>
         <ul>
           {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
         </ul>
-        <img src={country.flags.png} />
+        <img src={country.flags.png} alt={country.name.common}/>
+        <Weather country={country} />
+      </div>
+    )
+  }
+}
+
+const Weather = ({country}) => {
+  const api_key = process.env.REACT_APP_API_KEY
+  const [weather, setWeather] = useState(null)
+  const [lat, lon] = country.latlng
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
+      .then(response => {
+        console.log('promise fulfilled')
+        setWeather(response.data)
+      })
+      .catch(error => console.log(`Error: ${error}`))
+  }, [lat, lon, api_key])
+  
+  if (weather !== null) {
+    return (
+      <div>
+        <h2>Weather in {country.name.common}</h2>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].main}/>
+        {weather.weather[0].main}
+        <p><b>temperature:</b> {weather.main.temp} Celcius</p>
+        <p><b>wind:</b> {weather.wind.speed} mph</p>
       </div>
     )
   }
