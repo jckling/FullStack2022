36438fd8d957f6bf04ab0e42de7669b935b08b1b
jckling <jckling@163.com>
2022-05-28 13:25:06 +0800

    part3.3

--- index.js
+++ index.js
@@ -30,14 +30,25 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-
 app.get('/info', (req, res) => {
   const time = new Date();
   res.send(`<p>Phonebook has info for ${persons.length} people</p>
             <p>${time}</p>`)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
