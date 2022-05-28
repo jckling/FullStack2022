cb63fb8db92d6375b924b3dc621f8afbae845af8
jckling <jckling@163.com>
2022-05-28 14:57:09 +0800

    part3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 const log = morgan(':method :url :status :res[content-length] - :response-time ms :data')
