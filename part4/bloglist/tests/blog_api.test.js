38752b1304e044b88c64cfab54fc9082c4d62ba3
jckling <jckling@163.com>
2022-07-10 15:48:55 +0800

    part4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,8 +1,64 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -13,8 +69,21 @@ test('blogs are returned as json', async () => {
 
 test('there is only one blog', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(1)
+  expect(response.body).toHaveLength(6)
 })
+
+test('a blog has specific properties', async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+
+  expect(blog.id).toBeDefined()
+  expect(blog.title).toBeDefined()
+  expect(blog.author).toBeDefined()
+  expect(blog.url).toBeDefined()
+  expect(blog.likes).toBeDefined()
+  expect(blog._id).toBeUndefined()
+  expect(blog.__v).toBeUndefined()
+});
 
 afterAll(() => {
   mongoose.connection.close()
