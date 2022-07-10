bc2d404812fcce89e3c40b61adfbea2ab2f65696
jckling <jckling@163.com>
2022-07-10 20:07:37 +0800

    part4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -29,6 +29,7 @@ app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
+app.use(middleware.tokenExtractor)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -4,14 +4,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({})
@@ -27,11 +19,11 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'title and url missing' })
   }
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
+  
   const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
