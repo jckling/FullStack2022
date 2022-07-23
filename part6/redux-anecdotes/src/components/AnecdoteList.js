434a03196a78ae77cefa14f17d3a8d3e5de6e4d3
jckling <jckling@163.com>
2022-07-23 18:55:40 +0800

    part6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteOf } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return(
+    <div key={anecdote.id}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(voteOf(id))
+  }
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => vote(anecdote.id)}
+        />
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
