4cd14f83b6ab3b0d14521b2c5e11e466ab244ae7
jckling <jckling@163.com>
2022-07-23 18:41:34 +0800

    part6.5 & part6.6

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -32,9 +32,10 @@ const reducer = (state = initialState, action) => {
         ...anecdoteToVote,
         votes: anecdoteToVote.votes + 1,
       }
-      return state.map(anecdote =>
+      const newState = state.map(anecdote =>
         anecdote.id !== id ? anecdote : votedAnecdote
       )
+      return newState.sort((a, b) => b.votes - a.votes)
     case 'CREATE':
       return state.concat(action.data)
     default:
