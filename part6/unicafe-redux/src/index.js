21b51eb45ff747a9afd0704a11a9a7388c32d55e
jckling <jckling@163.com>
2022-07-23 18:09:38 +0800

    part6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,5 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import counterReducer from './reducer'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>good: {store.getState().good}</li>
+          <li>ok: {store.getState().ok}</li>
+          <li>bad: {store.getState().bad}</li>
+        </ul>
+      </div>
+      <button
+        onClick={e => store.dispatch({ type: 'GOOD' })}
+      >
+        good
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'OK' })}
+      >
+        ok
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'BAD' })}
+      >
+        bad
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'ZERO' })}
+      >
+        zero
+      </button>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
