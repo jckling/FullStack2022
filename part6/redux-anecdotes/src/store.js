6902908f58d1e3e2b5cda52cd5f0c5d9ee4e3c9a
jckling <jckling@163.com>
2022-07-24 14:09:48 +0800

    part6.13

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,14 +1,14 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import anecdoteReducer from "./reducers/anecdoteReducer"
 import notificationReducer from "./reducers/notificationReducer"
 import filterReducer from "./reducers/filterReducer"
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    notification: notificationReducer,
+    filter: filterReducer
+  }
 })
-
-const store = configureStore({ reducer: reducer})
 
 export default store
